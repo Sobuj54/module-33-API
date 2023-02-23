91955824c6f27967e617af4553df5ea767eb420a
sobuj <sobujahmed124@gmail.com>
2023-02-23 23:58:15 +0600

    advance inside index.js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function loadUsers2() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((Response) => Response.json())
+    .then((data) => displayUsers(data));
+}
+
+function displayUsers(data) {
+  const ul = document.getElementById("user-list");
+  for (const user of data) {
+    console.log(user.name);
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.appendChild(li);
+  }
+}
